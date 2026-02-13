578538e6bd688f141b4dc9ff568b029a17b1422a
maru <teawoo913@gmail.com>
2026-02-12 23:44:57 -0800

    fixed problems with navbar

--- src/main.js
+++ src/main.js
@@ -11,9 +11,11 @@ function sayHello() {
 const hamburger = document.getElementById("hamburger");
 const menu = document.getElementById("menu");
 
-hamburger.addEventListener("click", () => {
+hamburger.addEventListener("click", (e) => {
+  e.stopPropagation();
   menu.style.display = menu.style.display === "block" ? "none" : "block";
 });
+
 
 // Optional: close menu when clicking outside
 window.addEventListener("click", (e) => {
